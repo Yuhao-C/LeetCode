fd4702c81af279ae748c73e5697310c80e27bf8a
Yuhao Chen <yuhaochen0107@gmail.com>
2019-04-23 15:50:50 +0530

    first commit

---
+++ 070.climbing-stairs.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=70 lang=java
+ *
+ * [70] Climbing Stairs
+ */
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1, b = 1, c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        } 
+        return c;
+    }
+}
+
